b719fea52e1aa90fb12fa871954ec8ffdc902d9d
atef <atef.i.allam@gmail.com>
2024-12-30 02:34:57 +0200

    Welcome Home page

--- flask_LR_API.py
+++ flask_LR_API.py
@@ -17,31 +17,29 @@ with open('tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
 @app.route('/')
 def home():
     return render_template('home.html')
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
+    if request.method == 'GET':
+        return render_template('predict.html')
     
-    input_text = request.json['text']
+    if request.method == 'POST':
+        input_text = request.json.get('text')
 
-    # Transform the input text to TF-IDF features
-    input_tfidf = tfidf_vectorizer.transform([input_text])
+        # Transform the input text to TF-IDF features
+        input_tfidf = tfidf_vectorizer.transform([input_text])
+        prediction = model.predict(input_tfidf)
+        prediction_prob = model.predict_proba(input_tfidf)[0]
 
-    prediction = model.predict(input_tfidf)
-    
-    prediction_prob = model.predict_proba(input_tfidf)[0]
+        # Interpret the prediction: 1 is positive, 0 is negative
+        sentiment = "Positive" if prediction[0] == 1 else "Negative"
+        score = prediction_prob[1] if prediction[0] == 1 else prediction_prob[0]
 
-    # Interpret the prediction: 1 is positive, 0 is negative
-    if prediction[0] == 1:
-        sentiment = "Positive"
-        score = prediction_prob[1]  
-    else:
-        sentiment = "Negative"
-        score = prediction_prob[0] 
+        # Return the sentiment and score as JSON response
+        return jsonify({
+            'sentiment': sentiment,
+            'score': round(score, 4)
+        })
 
-    # Return the sentiment and score as JSON response
-    return jsonify({
-        'sentiment': sentiment,
-        'score': round(score, 4)
-    })
 
 if __name__ == '__main__':
     app.run(debug=True)
